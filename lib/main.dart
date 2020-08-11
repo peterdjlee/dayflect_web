9f114aacb75b85db927efb08336a0553553f6e78
Peter Lee <peterdjlee@gmail.com>
2020-08-10 20:11:22 -0400

    Build web

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
       title: 'Dayflect',
     );
   }
